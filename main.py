a344a61e197f1ebb1e0b92207a2e77af984fe31e
Diya116 <103575783+Diya116@users.noreply.github.com>
2022-04-30 23:31:16 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,57 +1,31 @@
 import cv2
 import pickle
-import cvzone
-import numpy as np
-#video feed
-cap = cv2.VideoCapture('carPark.mp4')
-
-with open('CarParkPos', 'rb') as f:
-    position = pickle.load(f)
-
+img = cv2.imread('carParkImg.png')
 width, height = 107, 48
-def checkParkingSpace(imgPro):
-    spaceCounter = 0
-    for pos in position:
-        x,y = pos
+try:
+        with open('CarParkPos', 'rb') as f:
+        position = pickle.load(f)
+except:
+        position = []
+def mouseclick(event,x,y,flags,parameters):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        position.append((x,y))
+    if event == cv2.EVENT_RBUTTONDOWN:
 
-        imgCrop = imgPro[y:y+height,x:x+width]
-       # cv2.imshow(str(x*y), imgCrop)
-        count = cv2.countNonZero(imgCrop)
-        cvzone.putTextRect(img, str(count), (x,y+height-3),scale = 1,
-                           thickness = 2, offset= 0, colorR = (0,0,255))
-        if count <900:
-            color = (0,255,0)
-            thickness = 5
-            spaceCounter +=1
-        else:
-            color = (0, 0, 255)
-
-            thickness = 2
-        cv2.rectangle(img, pos, (pos[0] + width, pos[1] + height), color, thickness)
-    cvzone.putTextRect(img, f'Free: {spaceCounter}/{len(position)}', (100,50), scale=3,
-                       thickness=5, offset=20, colorR=(0,200,0))
-
+        for i, pos in enumerate(position):
+            x1, y1 = pos
+            if x1 < x < x1 + width and y1 < y < y1 + height:
+                position.pop(i)
+    with open('CarParkPos', 'wb') as f:
+                pickle.dump(position,f)
 
 while True:
-    if cap.get(cv2.CAP_PROP_POS_FRAMES) == cap.get(cv2.CAP_PROP_FRAME_COUNT):
-        cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
-    success, img = cap.read()
-    imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    imgBlur = cv2.GaussianBlur(imgGray, (3,3), 1)
-    imgThreshold = cv2.adaptiveThreshold(imgBlur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
-                                         cv2.THRESH_BINARY_INV, 25, 16)
-    imgMedian = cv2.medianBlur(imgThreshold,5)
-    kernel = np.ones((3,3),np.int8)
-    imgDilate = cv2.dilate(imgMedian, kernel, iterations=1)
-    checkParkingSpace(imgDilate)
+    img  = cv2.imread('carParkImg.png')
+
+    for pos in position:
+        cv2.rectangle(img, pos, (pos[0] + width, pos[1] + height), (255, 0, 255), 3)
 
 
-
-
-
-
-
-    cv2.imshow('image', img)
-    #cv2.imshow('imageBlur', imgBlur)
-    #cv2.imshow('imageThres', imgMedian)
-    cv2.waitKey(10)
+    cv2.imshow("image",img)
+    cv2.setMouseCallback("image", mouseclick)
+    cv2.waitKey(1)
